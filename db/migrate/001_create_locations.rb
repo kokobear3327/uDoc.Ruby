f691dd9282d88d0f97415f183644dd89e5777714
bubbagump2020 <bubbagum2020@gmail.com>
2019-08-21 15:08:52 -0500

    search algorithm for City Location figured out and coded. Next up is by Doctor Specialty

--- db/migrate/001_create_locations.rb
+++
@@ -1,9 +0,0 @@
-class CreateLocations < ActiveRecord::Migration[4.2]
-	def change
-		create_table :locations do |table|
-			table.string :name
-			table.integer(:patient_id)
-			table.integer(:doctor_id)
-		end
-	end
-end

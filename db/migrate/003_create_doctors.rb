fa8923981e44093a14a54784551c81ddfee98beb
bubbagump2020 <bubbagum2020@gmail.com>
2019-08-22 16:17:00 -0500

    Updated Doctor Creation File

--- db/migrate/003_create_doctors.rb
+++ db/migrate/003_create_doctors.rb
@@ -4,6 +4,10 @@ class CreateDoctors < ActiveRecord::Migration[5.2]
             table.string :first_name
             table.string :last_name
             table.string :specialty
+            table.string :practice_name
+            table.integer :practice_phone_number
+            table.string :street_address
+            table.string :street_address_2
             table.string :city
             table.string :state
         end
